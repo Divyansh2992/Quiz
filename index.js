2dd6c96a8d6c02334c7a128655c668388223e75b
Divyansh2992 <Divyanshvijay92@gmail.com>
2024-07-27 12:24:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const mysqlconnection = require("./question.js");
+const app = express();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const path = require("path");
+const connection = require("./question.js");
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+    secret: 'quiz_game_secret',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+app.get('/', (req, res) => {
+    if (typeof req.session.score === 'undefined') {
+        req.session.score = 0;
+        req.session.questionsAnswered = 0;
+        req.session.askedQuestions = [];
+    }
+
+    let sql;
+    if (req.session.askedQuestions.length === 0) {
+        sql = 'SELECT * FROM questions ORDER BY RAND() LIMIT 1';
+    } else {
+        const askedQuestions = req.session.askedQuestions.join(',');
+        sql = `SELECT * FROM questions WHERE id NOT IN (${askedQuestions}) ORDER BY RAND() LIMIT 1`;
+    }
+
+    connection.query(sql, (err, result) => {
+        if (err) throw err;
+        if (result.length === 0) {
+            res.render('final', { finalScore: req.session.score });
+        } else {
+            req.session.askedQuestions.push(result[0].id);
+
+            // Increment the number of questions answered and set the question number
+            req.session.questionsAnswered += 1;
+            const questionNumber = req.session.questionsAnswered;
+
+            res.render('app', {
+                question: result[0],
+                score: req.session.score,
+                questionNumber: questionNumber
+            });
+        }
+    });
+});
+
+app.post('/submit', (req, res) => {
+    const userAnswer = req.body.answer;
+    const questionId = req.body.questionId;
+
+    const sql = 'SELECT * FROM questions WHERE id = ?';
+    connection.query(sql, [questionId], (err, result) => {
+        if (err) throw err;
+        const correctAnswer = result[0].answer;
+        const isCorrect = userAnswer === correctAnswer;
+
+        // Store the result of the question
+        if (!req.session.results) req.session.results = [];
+        req.session.results.push({
+            question: result[0].question,
+            userAnswer: userAnswer,
+            correctAnswer: correctAnswer,
+            isCorrect: isCorrect
+        });
+
+        if (isCorrect) {
+            req.session.score += 1;
+        }
+
+        res.redirect('/');
+    });
+});
+
+
+app.post('/stop', (req, res) => {
+    const finalScore = req.session.score;
+    const results = req.session.results || [];
+    req.session.destroy((err) => {
+        if (err) throw err;
+        res.render('final', { finalScore, results });
+    });
+});
+
+
+app.post('/start-again', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) throw err;
+        res.redirect('/');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at port:${port}`);
+});
---
+++ question.js
@@ -0,0 +1,28 @@
+const mysql=require("mysql2");
+const connection=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"123456",
+    database:"quiz_game",
+});
+
+connection.connect(function(err) {
+    if (err) {
+      console.error('Error connecting to database:', err.stack);
+      return;
+    }
+    console.log('Connected to database as ID:', connection.threadId);
+  
+    connection.query('SHOW TABLES', function(err, results, fields) {
+      if (err) {
+        console.error('Error fetching tables:', err.stack);
+        return;
+      }
+  
+      const tableNames = results.map(result => result[Object.keys(result)[0]]);
+
+      console.log('Tables in the database:', tableNames);
+    });
+});
+
+module.exports = connection;
